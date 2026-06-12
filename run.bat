4945c40317f9f4f0694cddc87bc48e0966e57020
sudodok <sudodok@example.com>
2026-06-12 23:28:17 +0700

    Remove pause and auto-close console in offline run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,3 @@
 @echo off
-title Sports Day Color Finance Launcher
-echo Opening Sports Day Color Finance System in your default browser...
 start "" "%~dp0index.html"
-echo System opened successfully!
-pause
+exit
